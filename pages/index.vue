d8bb4b0c00d749b309469f4f364b8b948264d345
Bioga Allen <allenojuka10@gmail.com>
2023-01-14 10:28:33 +0300

    Home page.

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="space-y-6">
+    <div class="">
+      <h1 class="font-bold text-xl">Updates</h1>
+    </div>
+
+    <div class="space-y-4">
+      <div class="border-b pb-4 border-black/10">
+        <div class="space-y-1">
+          <h3 class="text-lg font-semibold">@cyclic.sh/s3fs</h3>
+          <p class="text-sky-500 text-xs font-medium">January 20, 2023</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage',
+  // async asyncData({ $axios }) {
+  //   const data = await $axios.get('https://rn.cyclic.app/api/posts')
+  // }
+}
+</script>

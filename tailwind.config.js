8408ccae627a290fc82171a84b3f36cbcfd39b58
Bioga Allen <allenojuka10@gmail.com>
2023-01-14 10:26:53 +0300

    Init config.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extends: {
+      fontFamily: {
+        'sans': ['Inter'],
+        'display': ['Titillium Web'],
+      }
+    }
+  }
+}

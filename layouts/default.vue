b79695b9ef2fa0b411232cbe3ce533a2ba91a9be
Bioga Allen <allenojuka10@gmail.com>
2023-01-14 10:28:17 +0300

    Default layout.

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+
+    <div class="py-20 px-6 xl:px-0 lg:max-w-screen-lg mx-auto">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from '~/components/layout/Navbar.vue';
+
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>

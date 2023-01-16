0e894d36330ee8374e67037939d2a9824b2f9efb
Bioga Allen <allenojuka10@gmail.com>
2023-01-16 22:25:19 +0300

    Dynamic updates.

--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,10 @@
     </div>
 
     <div class="space-y-4">
-      <div class="border-b pb-4 border-black/10">
+      <div v-for="update of updates" :key="update.key" class="border-b pb-4 border-black/10">
         <div class="space-y-1">
-          <h3 class="text-lg font-semibold">@cyclic.sh/s3fs</h3>
-          <p class="text-sky-500 text-xs font-medium">January 20, 2023</p>
-        </div>
-      </div>
-      
-      <div class="border-b pb-4 border-black/10">
-        <div class="space-y-1">
-          <h3 class="text-lg font-semibold">Environment Variables in Build</h3>
-          <p class="text-sky-500 text-xs font-medium">January 20, 2023</p>
-        </div>
-      </div>
-      
-      <div class="border-b pb-4 border-black/10">
-        <div class="space-y-1">
-          <h3 class="text-lg font-semibold">Runtime Selection for New Apps</h3>
-          <p class="text-sky-500 text-xs font-medium">January 20, 2023</p>
+          <h3 class="text-lg font-semibold">{{ update.props.title }}</h3>
+          <p class="text-sky-500 text-xs font-medium">{{ new Date(update.props.created).toDateString() }}</p>
         </div>
       </div>
     </div>
@@ -33,9 +19,13 @@
 
 export default {
   name: 'IndexPage',
-  // async created() {
-  //   const data = await fetch('http://localhost:8000/api/posts')
-  //   console.log(data)
-  // }
+  async asyncData({ $config: { baseUrl } }) {
+    const data = await fetch(`${baseUrl}/api/posts`)
+      .then(res => res.json())
+
+    return {
+      updates: data
+    }
+  }
 }
 </script>

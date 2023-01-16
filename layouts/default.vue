590a3fb007a08963bc50eecd9947b83b46b8295d
Bioga Allen <allenojuka10@gmail.com>
2023-01-16 17:14:18 +0300

    Include footer, add min height.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,22 @@
   <div>
     <Navbar />
 
-    <div class="py-20 px-6 xl:px-0 lg:max-w-screen-lg mx-auto">
+    <div class="py-20 px-6 xl:px-0 lg:max-w-screen-lg mx-auto min-h-screen">
       <Nuxt />
     </div>
+
+    <Footer />
   </div>
 </template>
 
 <script>
   import Navbar from '~/components/layout/Navbar.vue';
+  import Footer from '~/components/layout/Footer.vue';
 
   export default {
     components: {
-      Navbar
+      Navbar,
+      Footer
     }
   }
 </script>

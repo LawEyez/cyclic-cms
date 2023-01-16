81b683668994dac4a98b51fd4b2b1c1f8ce0c3e8
Bioga Allen <allenojuka10@gmail.com>
2023-01-16 17:13:02 +0300

    Add footer.

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,3 +1,15 @@
 <template>
-  <footer class="">FOOTER</footer>
+  <footer class="bg-neutral-100">
+    <div class="py-10 px-6 xl:px-0 lg:max-w-screen-lg mx-auto">
+      <span class="text-xs font-semibold text-neutral-600">
+        &copy; Copyright 2023 | Cyclic Software
+      </span>
+    </div>
+  </footer>
 </template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>

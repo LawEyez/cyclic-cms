6527b74e4a56015455255fed7a66ca7983413f39
Bioga Allen <allenojuka10@gmail.com>
2023-01-19 11:28:58 +0300

    Add date.

--- pages/updates/_key.vue
+++ pages/updates/_key.vue
@@ -2,7 +2,12 @@
   <div class="">
     <div v-if="loading">Loading...</div>
     <div v-else class="space-y-6">
-      <h1 class="text-xl font-bold">{{ update.props.title }}</h1>
+      <div class="pb-4 border-b border-neutral-200 space-y-2">
+        <h1 class="text-xl font-bold">{{ update.props.title }}</h1>
+        <p class="text-sky-500 text-xs font-medium">
+              {{ new Date(update.props.created).toDateString() }}
+            </p>
+      </div>
       <div v-html="$md.render(update.props.content)" class="prose"></div>
       <!-- <nuxt-content :document="update.props.content" class="prose"></nuxt-content> -->
     </div>

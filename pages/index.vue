5da8f6865b8482e18c6702889c528805122b00f6
Bioga Allen <allenojuka10@gmail.com>
2023-01-19 10:11:37 +0300

    Remove prerendering.

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
       <h1 class="font-bold text-xl">Updates</h1>
     </div>
 
-    <div class="space-y-4">
+    <div v-if="loading">Loading...</div>
+    <div v-else class="space-y-4">
       <div v-for="update of updates" :key="update.key" class="border-b pb-4 border-black/10">
         <nuxt-link :to="`updates/edit/${update.key}`">
           <div class="space-y-1">
@@ -15,7 +16,7 @@
             </p>
           </div>
         </nuxt-link>
-        
+
         <nuxt-link class="text-xs capitalize" :to="`updates/${update.key}`">view</nuxt-link>
       </div>
     </div>
@@ -26,18 +27,32 @@
 
 export default {
   name: 'IndexPage',
-  async asyncData({ $config: { baseUrl } }) {
-    const data = await fetch(`${baseUrl}/api/posts`)
+  // async asyncData({ $config: { baseUrl } }) {
+  //   const data = await fetch(`${baseUrl}/api/posts`)
+  //     .then(res => res.json())
+
+  //   return {
+  //     updates: data
+  //   }
+  // },
+  // meta: {
+  //   cache: {
+  //     revalidate: true
+  //   }
+  // },
+  async created() {
+    this.loading = true
+    const data = await fetch(`${this.$config.baseUrl}/api/posts`)
       .then(res => res.json())
 
+    this.updates = data
+    this.loading = false
+  },
+  data() {
     return {
-      updates: data
+      loading: false,
+      updates: []
     }
-  },
-  meta: {
-    cache: {
-      revalidate: true
-    }
-  },
+  }
 }
 </script>

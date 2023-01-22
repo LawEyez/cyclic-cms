73b1a935cf1c9022663de9d985b3ed9a8dc9182f
Bioga Allen <allenojuka10@gmail.com>
2023-01-22 12:08:02 +0300

    Add delete.

--- pages/index.vue
+++ pages/index.vue
@@ -7,18 +7,27 @@
     <div v-if="loading">Loading...</div>
     <div v-else class="space-y-4">
       <div v-for="update of updates" :key="update.key" class="border-b pb-4 border-black/10">
-        <nuxt-link :to="`updates/edit/${update.key}`">
-          <div class="space-y-1">
-            <h3 class="text-lg font-semibold">{{ update.props.title }}</h3>
+        <div class="flex items-center justify-between">
+          <nuxt-link :to="`updates/edit/${update.key}`">
+            <div class="space-y-1">
+              <h3 class="text-lg font-semibold">{{ update.props.title }}</h3>
+  
+              <p class="text-sky-500 text-xs font-medium">
+                {{ new Date(update.props.created).toDateString() }}
+              </p>
+            </div>
 
-            <p class="text-sky-500 text-xs font-medium">
-              {{ new Date(update.props.created).toDateString() }}
-            </p>
-          </div>
-        </nuxt-link>
+          </nuxt-link>
+
+          <button v-on:click="() => handleDelete(update.key)" class="text-xs">
+            Delete
+          </button>
+        </div>
 
         <nuxt-link class="text-xs capitalize" :to="`updates/${update.key}`">view</nuxt-link>
       </div>
+
+      <div class="" v-if="updates.length === 0">No updates to show.</div>
     </div>
   </div>
 </template>
@@ -48,11 +57,32 @@ export default {
     this.updates = data
     this.loading = false
   },
+
   data() {
     return {
       loading: false,
       updates: []
     }
+  },
+
+  methods: {
+    async handleDelete(key) {
+      const username = prompt('Username')
+      const password = prompt('Password')
+
+      const options = {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
+        }
+      }
+
+      const data = await fetch(`${this.$config.baseUrl}/api/posts/delete/${key}`, options)
+        .then(res => res.json())
+      
+      console.log(data)
+    }
   }
+
 }
 </script>
